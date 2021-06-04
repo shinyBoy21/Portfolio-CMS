44f18bfbd5727f9ca6afc704bad8cce355612837
yves <yvesmut1000@gmail.com>
2021-06-04 15:38:22 +0100

    feat: add controllers

--- routes/admin.js
+++ routes/admin.js
@@ -1,28 +1,13 @@
 const express = require("express");
-const fs = require("fs");
+
 const path = require("path");
+const projectsController = require("../controllers/projects.js");
 
 const route = express.Router();
-const projects = [];
 
-route.get("/add-project", (req, res) => {
-  console.log(`in the first middleware`);
-  res.sendFile(path.join(__dirname, "..", "views", "add-project.html"));
-  res.render("add-project", {
-    title: "Add Project",
-    path: "/admin/add-project",
-  });
-  //console.log(path.join(__dirname,'../','views','add-product.html'))
-  // next();
-});
+route.get("/add-project", projectsController.getAddProject);
 
-route.post("/add-project", (req, res) => {
-  projects.push({ title: req.body.title });
-  console.log(projects);
-  //console.log(req.body);
-  fs.writeFileSync("outcome.txt", req.body.title);
-  res.redirect("/");
-});
+route.post("/add-project", projectsController.postProject);
 
 exports.route = route;
-exports.projects = projects;
+
--- routes/list.js
+++ routes/list.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const path = require("path");
-const adminData = require("./admin.js");
+const a = require("./admin.js");
+const projectsController = require("../controllers/projects.js");
 
-const projects = adminData.projects;
+const projects = projectsController.projects;
 
 const route = express.Router();
 
-route.get("/", (req, res) => {
-  console.log(projects);
-  //res.sendFile(path.join(__dirname, "../", "views", "projects.html"));
-  res.render("projects", { title: "Projects", projects: projects, path: "/" });
-});
+route.get("/", projectsController.getProjects);
 
 module.exports = route;
