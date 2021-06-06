4f17ae6782cb9f2a6e47d69a9e799fd5464ddd07
yves <yvesmut1000@gmail.com>
2021-06-06 13:59:52 +0100

    feat:get done with the MVC

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const express = require("express");
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 3000;
 const admin = require("./routes/admin");
 const projects = require("./routes/list.js");
 const errorController = require("./controllers/error");
